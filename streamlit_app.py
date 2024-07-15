94a1c020c85fd20e17c9d13e5d64210067b05cb9
EHPutlack <ethan.putlack@mitosenseinc.com>
2024-07-15 15:43:33 -0400

    Update streamlit_app.py
    reverted the import changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import cv2
-import os
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
