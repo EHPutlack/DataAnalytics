a7ff4edc5684580c47ad18040bb118ec1e264834
EHPutlack <ethan.putlack@mitosenseinc.com>
2024-07-22 14:13:18 -0400

    Update streamlit_app.py
    Small update to the Example Data section

--- streamlit_app.py
+++ streamlit_app.py
@@ -195,9 +195,14 @@ if menu_option == "Data Input":
                 st.write("Error: The uploaded CSV file does not contain the required columns.")
 
     elif data_input_option == "Example Data":
-        st.write("10 Example Patients")
-        example_data = create_realistic_data(10)
+    st.write("## View Example Patients")
+    
+    num_example_patients = st.number_input("Enter the number of example patients to view:", min_value=1, max_value=100, value=10)
+    
+    if st.button("Generate Example Data"):
+        example_data = create_realistic_data(num_patients=num_example_patients)
         st.dataframe(example_data)
+        
         st.write("## Predictions for example data")
         example_data_scaled = scaler.transform(example_data[parameters])
         model_choice = st.sidebar.selectbox("Choose a model", list(models.keys()))
