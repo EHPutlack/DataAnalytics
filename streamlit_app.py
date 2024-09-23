99892f072f15d23301b6eff8dd01be637212d455
EHPutlack <ethan.putlack@mitosenseinc.com>
2024-09-23 16:55:09 -0400

    Update streamlit_app.py
    Attempts to fix an error where pressing a button once would soft lock the user

--- streamlit_app.py
+++ streamlit_app.py
@@ -483,7 +483,6 @@ class ALSDetectionApp:
         """)
     
         if st.button("Get Started"):
-            st.session_state['menu_option'] = "Data Input"
             st.session_state['data_input_option'] = "File Upload"
             self.display_data_input()
 
